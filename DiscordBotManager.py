6612f0cf6193add2d202053a662642656da408c1
FailRANK <franknguyen2006@gmail.com>
2024-06-07 11:29:06 -0400

    Added and change GUI and cleaned up code

--- DiscordBotManager.py
+++ DiscordBotManager.py
@@ -2,9 +2,11 @@
 import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
-#Sets the window dimensions, title, and icon
+#Value used to hide discord token (very important to not show)
 Hidden = True
+#Sets the window dimensions, title, and icon
 Window = tk.Tk()
+Window.state("zoomed")
 Window.geometry("1000x750")
 Window.title("Discord Bot Manager")
 ico = Image.open('DiscordIcon.png')
@@ -13,6 +15,9 @@ Window.wm_iconphoto(False, photo)
 #Gets the theme that was saved
 ThemeData = open("Data/themedata.txt", "r").read()
 Token = open("Data/token.txt", "r").read()
+#Sets text
+BoldTextFont = ("Arial", 15, "bold")
+TextFont = ("Arial", 15)
 #See the command prefix and be able to change it
 def ShowCommandPrefix():
     Clear()
@@ -20,17 +25,17 @@ def ShowCommandPrefix():
     SetHeaderAndInfo("Data/commandprefix.txt", "Current Command Prefix", None)
 
     def SetCommandPrefix():
-        SetFileContent("Data/commandprefix.txt", "w", textbox.get('1.0', tk.END).strip())
+        SetFileContent("Data/commandprefix.txt", "w", NCPTB.get('1.0', tk.END).strip())
         ShowCommandPrefix()
 
-    label = tk.Label(Display, text="New Command Prefix", font=("Arial", 15, "bold"), bg=bgcolor, fg=fgcolor)
-    label.pack(pady = 10)
+    NCP = tk.Label(Display, text="New Command Prefix", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    NCP.pack()
 
-    textbox = tk.Text(Display, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor)
-    textbox.pack(pady = 10)
+    NCPTB = tk.Text(Display, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor)
+    NCPTB.pack(pady=5)
 
-    button = tk.Button(Display, text="Set", font=("Arial", 15, "bold"), command=SetCommandPrefix, bg=buttonbgcolor, fg=fgcolor)
-    button.pack(pady = 10)
+    NCPB = tk.Button(Display, text="Set", font=BoldTextFont, command=SetCommandPrefix, bg=buttonbgcolor, fg=fgcolor)
+    NCPB.pack()
 #See the commands and be able to change it
 def ShowCommands():
     Clear()
@@ -89,26 +94,30 @@ def ShowCommands():
         fr.close()
         fw.close()
         ShowCommands()
-    label = tk.Label(Display, text=" New Commands & Responses", font=("Arial", 15, "bold"), bg=bgcolor, fg=fgcolor)
-    label.pack(pady=10, padx=10)
+        
+    NCR = tk.Label(Display, text=" New Commands & Responses", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    NCR.pack()
 
     frame = tk.Frame(Display,bg=bgcolor)
-    frame.pack(pady=10, padx=10)
+    frame.pack(pady=5)
 
-    textbox1 = tk.Text(frame, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor, width=40)
-    textbox1.pack(padx = 10, pady = 10, side=tk.LEFT)
+    textbox1 = tk.Text(frame, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor, width=40)
+    textbox1.pack(padx = 10, side=tk.LEFT)
 
-    textbox2 = tk.Text(frame, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor, width=40)
-    textbox2.pack(padx = 10, pady = 10, side=tk.LEFT)
+    textbox2 = tk.Text(frame, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor, width=40)
+    textbox2.pack(padx = 10, side=tk.LEFT)
 
-    button = tk.Button(Display, text="Add", font=("Arial", 15, "bold"), command=SetCommands, bg=buttonbgcolor, fg=fgcolor)
-    button.pack(pady = 10)
+    button = tk.Button(Display, text="Set", font=BoldTextFont, command=SetCommands, bg=buttonbgcolor, fg=fgcolor)
+    button.pack()
     
-    textbox3 = tk.Text(Display, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor, width = 20)
-    textbox3.pack(pady = 10)
+    index = tk.Label(Display, text="Index", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    index.pack(pady = (10,0))
 
-    button5 = tk.Button(Display, text="Remove", font=("Arial", 15, "bold"), command=Remove, bg=buttonbgcolor, fg=fgcolor)
-    button5.pack(pady = 10)
+    textbox3 = tk.Text(Display, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor, width = 20)
+    textbox3.pack(pady = 5)
+
+    button5 = tk.Button(Display, text="Remove", font=BoldTextFont, command=Remove, bg=buttonbgcolor, fg=fgcolor)
+    button5.pack()
 #See the ready message and be able to change it
 def ShowStartup():
     Clear()
@@ -116,13 +125,18 @@ def ShowStartup():
     SetHeaderAndInfo("Data/on_ready.txt", "Current Start Up Message", None)
 
     def SetOn_ready():
-        Text = textbox.get('1.0', tk.END).strip()
+        Text = NSUMTB.get('1.0', tk.END).strip()
         SetFileContent("Data/on_ready.txt", "w")
         ShowStartup()
 
-    label = tk.Label(Display, text="New Start Up Message", font=("Arial", 15, "bold"), bg=bgcolor, fg=fgcolor).pack(pady = 10)
-    textbox = tk.Text(Display, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor).pack(pady = 10)
-    button = tk.Button(Display, text="Set", font=("Arial", 15, "bold"), command=SetOn_ready, bg=buttonbgcolor, fg=fgcolor).pack(pady = 10)
+    NSUM = tk.Label(Display, text="New Start Up Message", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    NSUM.pack()
+
+    NSUMTB = tk.Text(Display, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor)
+    NSUMTB.pack(pady = 5)
+
+    NSUMB = tk.Button(Display, text="Set", font=BoldTextFont, command=SetOn_ready, bg=buttonbgcolor, fg=fgcolor)
+    NSUMB.pack()
 #Send Message
 def ShowSendMessage():
     Clear()
@@ -131,38 +145,19 @@ def ShowSendMessage():
         if CMTB.get('1.0', tk.END).isspace():
             messagebox.showinfo("Error", "Invalid Input")
             return
-        message_file = open("Data/message.txt", "w")
-        message_file.write(CMTB.get('1.0', tk.END))
-        message_file.close()
-    
-    def delay():
-        if not DTB.get('1.0', tk.END).strip().isnumeric():
-            messagebox.showinfo("Error", "Invalid Input")
-            return
-        with open('Data/message.txt', 'r') as fr:
-            # reading line by line
-            lines = fr.readlines()
-            # pointer for position
-            ptr = 1
-        
-            # opening in writing mode
-            with open('Data/message.txt', 'w') as fw:
-                for line in lines:
-                    # we want to remove 5th line
-                    if ptr != 2:
-                        fw.write(line)
-                    else:
-                        fw.write(DTB.get('1.0', tk.END).strip())
-                    ptr += 1
-        fr.close()
-        fw.close()
+        messagetxt = open("Data/message.txt", "w")
+        messagetxt.write(CMTB.get('1.0', tk.END))
+        messagetxt.close()
 
-    frame = tk.Frame(Display,bg=bgcolor).pack(pady=10, padx=10)
-    CM = tk.Label(frame, text="Channel Message", font=("Arial", 15, "bold"), bg=bgcolor, fg=fgcolor).pack(pady = 10)
-    CMTB = tk.Text(frame, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor).pack(pady = 10)
-    SMB = tk.Button(frame, text="Send Message", command=message, font=("Arial", 15, "bold"), bg=buttonbgcolor, fg=fgcolor).pack(pady = 10)
-    #DTB = tk.Text(frame, text="Delay", font=("Arial", 15, "bold"), bg=textboxbgcolor, fg=fgcolor).pack(pady = 10)
-    DMB = tk.Button(frame, text="Set Delay", command=delay, font=("Arial", 15, "bold"), bg=buttonbgcolor, fg=fgcolor).pack(pady = 10)
+    CM = tk.Label(Display, text="Channel Message", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    CM.pack()
+
+    CMTB = tk.Text(Display, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor)
+    CMTB.pack(pady = 5)
+
+    SMB = tk.Button(Display, text="Send Message", command=message, font=BoldTextFont, bg=buttonbgcolor, fg=fgcolor)
+    SMB.pack()
+
 #Set up token
 def Setup():
     Clear()
@@ -180,27 +175,49 @@ def Setup():
             Hidden = True
         Setup()
 
+    SetHeaderAndInfo("Data/token.txt", "Discord Token", None)
+    if Hidden:
+        Info.config(text="*Hidden*")
+
     def SetToken():
-        Text = textboxt.get('1.0', tk.END).strip()
+        Text = CTTB.get('1.0', tk.END).strip()
         SetFileContent("Data/token.txt", "w", Text)
         Notice()
         Setup()
 
-    label = tk.Label(Display, text="Current Token", font=("Arial", 15, "bold"), bg=bgcolor, fg=fgcolor).pack(pady = 10)
-    textboxt = tk.Text(Display, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor).pack(pady = 10)
-    button = tk.Button(Display, text="Set", font=("Arial", 15, "bold"), command=SetToken, bg=buttonbgcolor, fg=fgcolor).pack(pady = 10)
-    hide = tk.Button(Display, text=show, font=("Arial", 15, "bold"), command=hidey, bg=buttonbgcolor, fg=fgcolor).pack(pady = 10)
-    label = tk.Label(Display, text="Current Channel ID", font=("Arial", 15, "bold"), bg=bgcolor, fg=fgcolor).pack(pady = 10)
-    textbox = tk.Text(Display, height=1, font=("Arial", 15), bg=textboxbgcolor, fg=fgcolor).pack(pady = 10)
-    button = tk.Button(Display, text="Set Channel ID", font=("Arial", 15, "bold"), command=SetToken, bg=buttonbgcolor, fg=fgcolor).pack(pady = 10)
+    def SetChannel():
+        Text = CCITB.get('1.0', tk.END).strip()
+        SetFileContent("Data/channelid.txt", "w", Text)
+        Setup()
 
-    tokentxt = open("Data/token.txt", "r")
-    if Hidden:
-        Info.config(text="*Hidden*")
-    else:
-        Info.config(text=tokentxt.read())
-    tokentxt.close()
-    Header.config(text="Discord Token")
+    CT = tk.Label(Display, text="Current Token", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    CT.pack()
+
+    CTTB = tk.Text(Display, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor)
+    CTTB.pack(pady = 5)
+
+    CTB = tk.Button(Display, text="Set", font=BoldTextFont, command=SetToken, bg=buttonbgcolor, fg=fgcolor)
+    CTB.pack()
+
+    CTB2 = tk.Button(Display, text=show, font=BoldTextFont, command=hidey, bg=buttonbgcolor, fg=fgcolor)
+    CTB2.pack(pady=(5,10))
+
+    CCI = tk.Label(Display, text="Current Channel ID", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    CCI.pack()
+
+    file=open("Data/channelid.txt", "r")
+    CCI2 = tk.Label(Display, text=file.read().strip(), font=TextFont, bg=bgcolor, fg=fgcolor)
+    CCI2.pack(pady=(0,10))
+
+    NCCITB = tk.Label(Display, text="Current Channel ID", font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    NCCITB.pack()
+
+    CCITB = tk.Text(Display, height=1, font=TextFont, bg=textboxbgcolor, fg=fgcolor)
+    CCITB.pack(pady = 5)
+
+    CCIB = tk.Button(Display, text="Set Channel ID", font=BoldTextFont, command=SetChannel, bg=buttonbgcolor, fg=fgcolor)
+    CCIB.pack()
+
 #Changes window theme
 def ChangeTheme():
     global ThemeData
@@ -214,9 +231,13 @@ def ChangeTheme():
         Data = open("Data/themedata.txt", "w")
         Data.write("Light")
         Data.close()
-    SetTheme()
+    for i in Window.winfo_children():
+        i.destroy()
+    Main()
 #Clear window elements
 def Clear():
+    Header.config(text="")
+    Info.config(text="")
     for i in Display.winfo_children():
         i.destroy()
 #Sets file content
@@ -227,58 +248,36 @@ def SetFileContent(name, type, content):
 #Sets the header and info
 def SetHeaderAndInfo(name, title, special):
     if special:
-        file1 = open(name[0], "r")
-        file2 = open(name[1], "r")
+        file1, file2 = open(name[0], "r"), open(name[1], "r")
         Header.config(text=title)
-        content = file2.readlines()
-        line = 1
+        lines, line = file2.readlines(), 1
         for i in file1.readlines():
-            Info.config(text=Info.cget('text') + "[" + str(line) + "] " + i.strip() + " " + "| " + content[line-1])
+            Info.config(text=Info.cget('text') + "[" + str(line) + "] " + i.strip() + " " + "| " + lines[line-1])
             line+=1
-        file1.close()
-        file2.close()
+        file1.close(), file2.close()
         return
     file=open(name, "r")
     Header.config(text=title)
     Info.config(text=file.read().strip())
     file.close()
+
 #Notice
 def Notice():
     if Token != "":
         SP.config(text="Setup")
     else:
         SP.config(text="Setup [!]")
-Notice()    
-#Sets up all the necessary GUI elements
-Title = tk.Label(Window, text="Discord Bot Manager", font=("Arial", 30, "bold"))
-Title.pack(pady = 10)
-ButtonFrame = tk.Frame(Window)
-ButtonFrame.pack(pady=10)
-CPB = tk.Button(ButtonFrame, text="Command Prefix", font=("Arial", 15, "bold"), command=ShowCommandPrefix)
-CPB.pack(padx=10,pady=10, side=tk.LEFT)
-CB = tk.Button(ButtonFrame, text="Commands & Responses", font=("Arial", 15, "bold"), command=ShowCommands)
-CB.pack(padx=10,pady=10, side=tk.LEFT)
-ORMB = tk.Button(ButtonFrame, text="Start Up Message", font=("Arial", 15, "bold"), command=ShowStartup)
-ORMB.pack(padx=10,pady = 10, side=tk.LEFT)
-SCMB = tk.Button(ButtonFrame, text="Send Message", font=("Arial", 15, "bold"), command=ShowSendMessage)
-SCMB.pack(padx=10,pady = 10, side=tk.LEFT)
-Header = tk.Label(Window, font=("Arial", 15, "bold"))
-Header.pack(pady = 10)
-Info = tk.Label(Window, font=("Arial", 15))
-Info.pack(pady = 10)
-Display = tk.Frame(Window)
-Display.pack(pady=10)
-Theme = tk.Button(Window, text="Theme: " + ThemeData, command=ChangeTheme, font=("Arial", 15, "bold"))
-Theme.pack(side=tk.BOTTOM)
-SP = tk.Button(ButtonFrame, command=Setup, font=("Arial", 15, "bold"))
-SP.pack(padx = 10, pady = 10, side=tk.LEFT)
 
-#Sets window theme
-def SetTheme():
+def Main():
+    global SP
+    global Header
+    global Info
+    global Display
     global bgcolor
     global fgcolor
     global buttonbgcolor
     global textboxbgcolor
+    #Sets Theme
     if ThemeData == "Light":
         bgcolor = "#ffffff"
         fgcolor = "#000"
@@ -289,19 +288,45 @@ def SetTheme():
         fgcolor = "#fff"
         buttonbgcolor = "#232428"
         textboxbgcolor = "#383a40"
+    
+    #Sets up all the necessary GUI elements
     Window.config(bg=bgcolor)
-    Title.config(bg=bgcolor, fg=fgcolor)
-    ButtonFrame.config(bg=bgcolor)
-    CPB.config(bg=buttonbgcolor, fg=fgcolor)
-    CB.config(bg=buttonbgcolor, fg=fgcolor)
-    ORMB.config(bg=buttonbgcolor, fg=fgcolor)
-    SCMB.config(bg=buttonbgcolor, fg=fgcolor)
-    Header.config(bg=bgcolor, fg=fgcolor)
-    Info.config(bg=bgcolor, fg=fgcolor)
-    Display.config(bg=bgcolor)
-    Theme.config(text="Theme: " + ThemeData,bg=buttonbgcolor, fg=fgcolor)
-    SP.config(bg=buttonbgcolor, fg=fgcolor)
 
-SetTheme()
+    Title = tk.Label(Window, text="Discord Bot Manager", font=("Arial", 30, "bold"), bg=bgcolor, fg=fgcolor)
+    Title.pack(pady = 20)
+
+    ButtonFrame = tk.Frame(Window, bg=bgcolor)
+    ButtonFrame.pack()
+
+    CPB = tk.Button(ButtonFrame, text="Command Prefix", font=BoldTextFont, command=ShowCommandPrefix, bg=buttonbgcolor, fg=fgcolor)
+    CPB.pack(padx=10, side=tk.LEFT)
+
+    CB = tk.Button(ButtonFrame, text="Commands & Responses", font=BoldTextFont, command=ShowCommands, bg=buttonbgcolor, fg=fgcolor)
+    CB.pack(padx=10, side=tk.LEFT)
+
+    ORMB = tk.Button(ButtonFrame, text="Start Up Message", font=BoldTextFont, command=ShowStartup, bg=buttonbgcolor, fg=fgcolor)
+    ORMB.pack(padx=10, side=tk.LEFT)
+
+    SCMB = tk.Button(ButtonFrame, text="Send Message", font=BoldTextFont, command=ShowSendMessage, bg=buttonbgcolor, fg=fgcolor)
+    SCMB.pack(padx=10, side=tk.LEFT)
+
+    SP = tk.Button(ButtonFrame, command=Setup, font=BoldTextFont, bg=buttonbgcolor, fg=fgcolor)
+    SP.pack(padx = 10, side=tk.LEFT)
+
+    Header = tk.Label(Window, font=BoldTextFont, bg=bgcolor, fg=fgcolor)
+    Header.pack(pady=(20,0))
+
+    Info = tk.Label(Window, font=TextFont, bg=bgcolor, fg=fgcolor)
+    Info.pack(pady=(0,10))
+
+    Display = tk.Frame(Window, bg=bgcolor)
+    Display.pack()
+
+    Theme = tk.Button(Window, text="Theme: " + ThemeData, command=ChangeTheme, font=BoldTextFont, bg=buttonbgcolor, fg=fgcolor)
+    Theme.pack(side=tk.BOTTOM)
+
+    Notice()
+
+Main()
 
 Window.mainloop()
